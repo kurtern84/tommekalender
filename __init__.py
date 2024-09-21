84e90200767632d1bfd1094bbc5d7b936a3af715
Kurt Herman Frydenberg <kurtern84@gmail.com>
2024-09-21 07:53:35 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,12 @@
 """The tommekalender component."""
 import logging
-
-from homeassistant.helpers.typing import HomeAssistantType
+from homeassistant.core import HomeAssistant  # Oppdatert import
 
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "tommekalender"
 
-async def async_setup(hass: HomeAssistantType, config: dict):
+async def async_setup(hass: HomeAssistant, config: dict):  # Endret HomeAssistantType til HomeAssistant
     """Set up the Tommekalender component."""
     _LOGGER.info("Setting up Tommekalender")
     return True
